4a4a1f939ee177b69f9fa3fa8fa8ea87f088dfb4
Nematulloh <unn.info.tech@gmail.com>
2023-08-24 16:03:22 +0300

    register, login and log out

---
+++ apis/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+
+    def __str__(self):
+        return self.username
